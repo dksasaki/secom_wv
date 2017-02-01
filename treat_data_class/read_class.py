ad18796130f595d1e29426be5c736169433289da
dksasaki <dalton.sasaki@gmail.com>
2017-02-01 14:16:35 -0200

    treat_data_class/

---
+++ treat_data_class/read_class.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime
+
+class Pnboia_read(object):
+	def __init__(self,name):
+		self.pnboia = pd.read_csv(name)
+
+	def get_attributes_from_data(self):
+		for key in self.pnboia.keys():
+				setattr(self,key,self.pnboia[key])
+
+	def remove_repeated(self,var):
+		varb = list(set(zip(self.dt,var)))
+		varb.sort()
+		return varb
+
+	def datetime(self,year='yyyy',month=' mm',day=' dd',hour=' hour',minu=' min'):
+		y = self.pnboia[year]
+		m = self.pnboia[month]
+		d = self.pnboia[day]
+		h = self.pnboia[hour]
+		mi= self.pnboia[minu]
+
+		self.dt = []
+		for i in zip(y,m,d,h,mi):
+			dt = '%04d-%02d-%02d %02d:%02d:00' % (i[0],i[1],i[2],i[3],i[4])
+			self.dt.append(datetime.strptime(dt, '%Y-%m-%d %H:%M:%S'))
+
+if __name__ == '__main__':
+	pass
---
+++ treat_data_class/treat_data_class.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime
+
+
+class Filters(object):
+	def __init__(self,np=11,fill_value=-99999):
+		self.n = n
+		self.fill_value = fill_value
+
+	def filter_median_data(self,var):
+		if self.n % 2 == 1:
+			varb = []
+			n1   = self.n
+			n2   = self.n+1
+			for i in range(n1,np.size(var)-n2):
+				varb.append(np.nanmedian(var[i-n1:i+n2]))
+			return varb
+		else:
+			print 'n must be even'
+
+	def substitute_for_nan_values(self,var):
+		varb           = np.array(var)
+		varb[varb==self.fill_value] = np.nan
+		return varb
+
+	def cut_interval_by_index(self,var):
+		n1   = self.n
+		n2   = self.n+1
+
+		varb = var[n1:-n2]
+		return varb
+
+if __name__ == '__main__':
+	from pydap.client import open_url
+	import matplotlib.pyplot as pl
+	import xarray as xr
+	buoy = xr.open_dataset('http://opendap.saltambiental.com.br:8080/pnboia/Bsantos.nc')
+
+	f  = Filters()
+	v  = buoy['wave_hs'].data
+	v1 = f.substitute_for_nan_values(v)
+	v2 = f.filter_median_data(v1)
+	d  = f.cut_interval_by_index(buoy['time'])
+
+	plt.figure()
+	plt.plot(d,v2)
