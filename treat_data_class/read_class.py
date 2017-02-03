042b1b95ce55581babfc3d8b592bf605b8e09be4
dksasaki <dalton.sasaki@gmail.com>
2017-02-03 16:26:25 -0200

    comments included for read_class

--- treat_data_class/read_class.py
+++ treat_data_class/read_class.py
@@ -1,22 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+from pandas import read_csv
+from xarray import open_dataset
 from datetime import datetime
 
 class Pnboia_read(object):
-	def __init__(self,name):
-		self.pnboia = pd.read_csv(name)
+	def __init__(self):
+		"""
+		Reads and give a basic treatment to Pnboia dataset
+		self.read_csv : read a Pnboia csv  (it uses pandas)
+		self.read_csv : read a netcdf file (it uses xarray)
+		"""
+
+		self.read_csv = read_csv
+		self.open_dataset = open_dataset
+		self.pnboia   = None
+
 
 	def get_attributes_from_data(self):
+		"""
+		get every key form dataset and return as a class attribute
+		"""
 		for key in self.pnboia.keys():
 				setattr(self,key,self.pnboia[key])
 
 	def remove_repeated(self,var):
+		"""
+		remove the repeated dates from the combined lists of self.dt and var
+		self.dt attribute is defined by self.datetime method
+		"""
 		varb = list(set(zip(self.dt,var)))
 		varb.sort()
 		return varb
 
 	def datetime(self,year='yyyy',month=' mm',day=' dd',hour=' hour',minu=' min'):
+		"""
+		create a date list using the time keys of the file
+		"""
 		y = self.pnboia[year]
 		m = self.pnboia[month]
 		d = self.pnboia[day]
@@ -30,6 +50,7 @@ class Pnboia_read(object):
 
 if __name__ == '__main__':
 	file ='file'
-	pn = Pnboia_read(file)
+	pn = Pnboia_read()
+	pn.read_csv(file)
 	pn.get_attributes_from_data()
 	pn.datetime()
